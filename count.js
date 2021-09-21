931865863f6ceb1aa8d440ab2d0ef93550d6e758
Austin Wang <59949727+austinwang23@users.noreply.github.com>
2021-09-20 22:27:25 -0700

    Add files via upload

---
+++ count.js
@@ -0,0 +1,56 @@
+import { readdir } from 'fs/promises';
+const path = './inbox';
+function checkCall(message) {
+  return message.type === 'Call';
+}
+async function gett() {
+  let final = [];
+  try {
+    const dirs = await readdir(path, {withFileTypes: true});
+    for (const dir of dirs) {
+      if(dir.isDirectory()){
+        let numFiles = 0;
+        let numMsgs = 0;
+        let seconds = 0;
+        let name = "";
+        const subpath = `./inbox/${dir.name}`;
+        const files = await readdir(subpath, {withFileTypes: true});
+        for (const file of files) {  
+          if(!file.isDirectory()){
+            if(file.name !== ".DS_Store"){
+              numFiles++;
+              const fullpath = `${subpath}/message_${numFiles}.json`
+              const json = await import(fullpath);
+              name = json.default.title;
+              numMsgs += json.default.messages.length;
+              const messages = json.default.messages;
+              const calls = messages.filter(checkCall);
+              calls.forEach((call) => {
+                seconds += call.call_duration;
+              });
+            }
+          }
+        }
+        if(numFiles>1){
+          var h = Math.floor(seconds / 3600);
+          var m = Math.floor((seconds % 3600) / 60);
+          var s = Math.floor((seconds % 3600) % 60);
+
+          var hDisplay = h > 0 ? h + 'h ' : ' ';
+          var mDisplay = m > 0 ? m + 'm ' : ' ';
+          var sDisplay = s > 0 ? s + 's' : '';
+          const callString = hDisplay + mDisplay + sDisplay;
+          final.push({ msgs: numMsgs, name: name, totalCall: callString});
+        }
+      }
+    }
+    final.sort(function (a, b) {
+      return a.msgs - b.msgs;
+    });
+    console.log(final)
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+gett();
